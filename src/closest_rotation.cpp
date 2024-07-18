44d5978176020342ab1f64031375df896a7f4b6d
Arvind Rao <arvsrao@gmail.com>
2024-07-19 01:20:16 +0200

    5th & 6th tasks

--- src/closest_rotation.cpp
+++ src/closest_rotation.cpp
@@ -1,9 +1,15 @@
 #include "closest_rotation.h"
+#include <Eigen/SVD>
+
+using Eigen::DecompositionOptions::ComputeThinU;
+using Eigen::DecompositionOptions::ComputeThinV;
 
 void closest_rotation(
   const Eigen::Matrix3d & M,
   Eigen::Matrix3d & R)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
+    Eigen::JacobiSVD<Eigen::Matrix3d> svd(M, ComputeThinU | ComputeThinV);
+    Eigen::Matrix3d O;
+    O << 1,0,0, 0,1,0, 0,0,(svd.matrixU() * svd.matrixV().transpose()).determinant();
+    R = svd.matrixU() * O * svd.matrixV().transpose();
 }
--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -7,8 +7,16 @@ void point_to_point_rigid_matching(
   Eigen::Matrix3d & R,
   Eigen::RowVector3d & t)
 {
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+    double N = X.rows();
+    Eigen::RowVector3d x_bar, p_bar;
+    Eigen::MatrixXd M;
+
+    x_bar = (1/N) * X.colwise().sum();
+    p_bar = (1/N) * P.colwise().sum();
+
+    M = (P.rowwise() - p_bar).transpose() * X.rowwise() - x_bar;
+    closest_rotation(M, R);
+    t = p_bar.transpose() - R * x_bar.transpose();
+
 }
 
